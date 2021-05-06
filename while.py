938fc1da042250bd61de5a5da67e7b08e953cc00
ashwith goud <aariktsen@gmail.com>
2021-05-06 16:59:32 +0530

    while loop example

---
+++ while.py
@@ -0,0 +1,25 @@
+i=1
+while i<=5:
+    print("barcelona",end="")
+    j=1
+    while j <=5:
+        print(" la liga",end ="")
+        j=j+1
+    i=i+1
+    print()
+x=1
+while x<=100:
+    if x %3 ==0 or x % 5 == 0:
+        pass
+    else:
+        print(x)
+    x=x+1
+    print()
+a=1
+while a<5:
+    b=1
+    while b<=5:
+        print("#",end ="")
+        b=b+1
+    a=a+1
+    print()
